9130efb4514fbeffd1f7731768cc371b35a6a874
sunnyko3 <57108786+sunnyko3@users.noreply.github.com>
2019-11-07 17:09:37 -0700

    updated work

--- practice.js
+++ practice.js
@@ -17,7 +17,10 @@ var arr = [10,20,30];
   Return the first item in the given array.
 */
 
-//Code Here
+function first(arr){
+      return arr[0]
+    }
+
 
 
 
@@ -32,7 +35,9 @@ var arr = [40,50,60];
   Return the last item in the given array.
 */
 
-//Code Here
+function last(arr){
+  return arr[arr.length -1]
+}
 
 
 
@@ -47,8 +52,11 @@ var family = ['Tyler', 'Jordyn', 'Ryan', 'Chelsey', 'Ireland'];
   Loop through the given array and alert every item in the array.
 */
 
-//Code Here
-
+function looper(family){
+  for(let i=0; i < family.length; i++){
+    alert(family[i])
+  }
+}
 
 
 ////////// PROBLEM 4 //////////
@@ -62,7 +70,13 @@ var letters = ['A', 'B', 'C', 'D', 'E'];
   Loop backwards, starting at the end of the letters array, alerting every item in the array.
 */
 
-//Code Here
+// function reversedLooper()
+
+function reversedLooper(letters){
+  for(let i = letters.length-1; i >= 0; i--){
+    alert(letters[i])
+  }
+}
 
 
 
